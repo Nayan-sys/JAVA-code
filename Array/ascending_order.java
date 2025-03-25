2982beaae1e9cd6699cacd695cfbf54ef5948e22
Nayan <nayansrivastav337@gmail.com>
2025-03-26 04:04:26 +0530

    check array is in which order

---
+++ Array/ascending_order.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class ascending_order {
+    Scanner sc=new Scanner(System.in);
+       System.out.print("Enter the size of an array: ");
+       int size=sc.nextInt();
+       int num[]=new int[size];
+//        for taking input
+       for(int i=0;i<num.length;i++){
+           num[i]=sc.nextInt();
+       }
+//
+       boolean isAscending = true;
+
+       for(int i=0; i<num.length-1; i++) { // NOTICE numbers.length - 1 as termination condition
+           if(num[i] > num[i+1]) { // This is the condition for descending order
+               isAscending = false;
+           }
+       }
+
+       if(isAscending) {
+           System.out.println("The array is sorted in ascending order");
+       } else {
+           System.out.println("The array is not sorted in ascending order");
+       }
+
+}
