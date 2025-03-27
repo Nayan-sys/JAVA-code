bef02da326010ac15eb8b284058d65ae8f4a7143
Nayan <nayansrivastav337@gmail.com>
2025-03-28 00:14:35 +0530

    Binary Search

---
+++ 2D_Array.java/Binary_search.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Binary_search {
+
+    
+//        Take input of elements from the user and search the element that occur int that array at which index:
+
+public static void main(String[]args){
+
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter the row of an array: ");
+       int rows=sc.nextInt();
+       System.out.println("Enter the column of an array: ");
+       int cols=sc.nextInt();
+       int num[][]=new int[rows][cols];
+//        for input:
+       System.out.println("Enter the elements of an array:");
+       for(int i=0;i<rows;i++){
+           for(int j=0;j<cols;j++){
+               num[i][j]=sc.nextInt();
+           }
+       }
+       System.out.println("Enter the element u want to search:");
+       int n=sc.nextInt();
+//        for search of the elements:
+       for(int i=0;i<rows;i++){
+           for(int j=0;j<cols;j++){
+               if(n==num[i][j]){
+                   System.out.println("The element is found at (" + i +","+ j + ")");
+               }
+           }
+       }
+    }
+    
+}
