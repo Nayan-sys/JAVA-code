20e43ceed6c5c4eb17b94a6182e8657275812abb
Nayan <nayansrivastav337@gmail.com>
2025-03-31 22:01:10 +0530

    Star pint in order

---
+++ Pattern_Print/Barfi.java
@@ -0,0 +1,29 @@
+public class Barfi {
+    public static void main(String[]args){
+    int n = 3;
+    for (int i = 1; i <= n; i++) {
+        for (int s = 1; s <= (n - i); s++) {
+            System.out.print(" ");
+        }
+        for (int j = i; j>=1; j--) {
+            System.out.print("*");
+        }
+        for (int j = 2; j <=i; j++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    for (int i = n; i>=1; i--) {
+        for (int s = 1; s <= (n - i); s++) {
+            System.out.print(" ");
+        }
+        for (int j = i; j >= 1; j--) {
+            System.out.print("*");
+        }
+        for (int j = 2; j <= i; j++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+}
