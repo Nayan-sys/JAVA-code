78842ae570f05ff018aa87e674807ae8d42c3b51
Nayan <nayansrivastav337@gmail.com>
2025-04-06 22:07:23 +0530

    Two sum of an array

---
+++ Array/Two_sum.java
@@ -0,0 +1,14 @@
+class Two_sum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+
+        // This line should never be reached since a solution is guaranteed
+        return new int[0];
+    }
+}
